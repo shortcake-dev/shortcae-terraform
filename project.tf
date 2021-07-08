cd4ed2268bc5904b79d876a00b862ddd25879e41
ignormies <bryce.beagle@gmail.com>
2021-07-08 10:46:07 -0700

    Don't use credentials attribute anymore

--- project.tf
+++ project.tf
@@ -1,11 +1,9 @@
 provider "google" {
-  credentials = file("credentials.json")
   project     = var.project_id
   region      = var.region
 }
 
 provider "google-beta" {
-  credentials = file("credentials.json")
   project     = var.project_id
   region      = var.region
 }
