260c38181659811182ed8a067539a268fe0a6d89
ignormies <bryce.beagle@gmail.com>
2021-08-02 23:25:04 -0700

    Support deployment_name in tf config

--- shortcake-backend.tf
+++ shortcake-backend.tf
@@ -1,26 +1,29 @@
 locals {
-  service_name = "${var.project_id}-backend"
+  deployment_name = join(
+    "-",
+    compact([var.project_id, var.release_type, var.deployment_name])
+  )
 }
 
 module "network" {
   source = "./modules/network"
 
-  network_name = local.service_name
+  network_name = local.deployment_name
 }
 
 module "docker_registry" {
   source = "./modules/docker_registry"
 
-  region = var.region
-  repository_id = local.service_name
+  region        = var.region
+  repository_id = local.deployment_name
 }
 
 module "cloud_run" {
   source = "./modules/cloud_run"
 
-  service_name = local.service_name
-  region = var.region
-  image = "us-docker.pkg.dev/cloudrun/container/hello"
+  service_name = local.deployment_name
+  region       = var.region
+  image        = "us-docker.pkg.dev/cloudrun/container/hello"
 
   sql_instance = module.database.database
 }
@@ -29,10 +32,10 @@ module "database" {
   source = "./modules/database"
 
   region = var.region
-  vpc = module.network.vpc
+  vpc    = module.network.vpc
 
-  database_name = local.service_name
-  tier = "db-f1-micro"
+  database_name = local.deployment_name
+  tier          = "db-f1-micro"
 
   deletion_protection = (var.release_type == "prod")
 
--- variables.tf
+++ variables.tf
@@ -1,14 +1,20 @@
+variable "project_id" {
+  type = string
+  default = "shortcake"
+}
+
 variable "release_type" {
   type = string
   default = "dev"
 }
 
+variable "deployment_name" {
+  description = "Auxiliary suffix to append to release for resource names"
+  type = string
+  default = null
+}
+
 variable "region" {
   type = string
   default = "us-west1"
-}
-
-variable "project_id" {
-  type = string
-  default = "shortcake-dev"
 }
