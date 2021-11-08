b6530059cbc1ac7b63a8ef67ca04e9829274963e
ignormies <bryce.beagle@gmail.com>
2021-11-08 14:00:31 -0800

    Use triggers to store variables as a workaround

--- modules/docker_image/main.tf
+++ modules/docker_image/main.tf
@@ -5,24 +5,26 @@ locals {
   google_registry  = "${local.google_subdomain}.pkg.dev/${var.project}/${var.docker_registry.repository_id}"
 
   dockerhub_registry = "registry.hub.docker.com/${var.dockerhub_repo}"
-
-  dockerhub_image = "${local.dockerhub_registry}/${complete_image_name}"
-  google_image    = "${local.google_registry}/${complete_image_name}"
 }
 
 resource "null_resource" "docker_image" {
+  triggers = {
+    # https://github.com/hashicorp/terraform/issues/23679
+    dockerhub_image = "${local.dockerhub_registry}/${complete_image_name}"
+    google_image    = "${local.google_registry}/${complete_image_name}"
+  }
 
   provisioner "local-exec" {
     command = <<-EOT
-      docker pull ${local.dockerhub_image}
-      docker tag ${local.dockerhub_image} ${local.google_image}
-      docker push ${local.google_image}
+      docker pull ${self.triggers.dockerhub_image}
+      docker tag ${self.triggers.dockerhub_image} ${self.triggers.google_image}
+      docker push ${self.triggers.google_image}
     EOT
   }
 
   provisioner "local-exec" {
     when    = destroy
-    command = "gcloud artifacts docker images delete ${local.google_image}"
+    command = "gcloud artifacts docker images delete ${self.triggers.google_image}"
   }
 }
 
