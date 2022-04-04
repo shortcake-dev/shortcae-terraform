27e8f093fdf5ec0e11a9c8c2362b8fa6b47ba5ba
ignormies <bryce.beagle@gmail.com>
2022-04-03 17:44:31 -0700

    Use random # in service account ID

--- modules/service_account/main.tf
+++ modules/service_account/main.tf
@@ -2,7 +2,7 @@ resource "random_uuid" "account_id" {
 }
 
 resource "google_service_account" "service_account" {
-  account_id   = "sa-${random_uuid.account_id.result}"
+  account_id   = "sa-${substr(random_uuid.account_id.result, -8, -1)}-${var.deployment_name}"
   display_name = "${var.name}-${var.deployment_name}"
 }
 
